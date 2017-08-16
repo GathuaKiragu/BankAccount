d887e4afebfe3a5b5c5d40682b07e4f676a5722d
Gathua Kiragu <kiragugathua@gmail.com>
2017-08-16 03:18:47 +0300

    Added an instanciate method Method to be excecuted before each test in the class

--- src/main/java/Deposit.java
+++ src/main/java/Deposit.java
@@ -1,3 +1,12 @@
+
 public class Deposit{
-  
+  private int id;
+  private int accountNumber;
+  private double amount;
+
+  // Constructor
+  public Deposit(int accountNumber, double amount){
+    this.accountNumber=accountNumber;
+    this.amount=amount;
+  }
 }
--- src/test/java/DepositTest.java
+++ src/test/java/DepositTest.java
@@ -1,3 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
 public class DepositTest{
-  
+
+  private Deposit initialDeposit;
+
+// this method is executed before each test in the class, so as to execute some pre-conditions necessary for the test
+  @Before
+  public void instanciate(){
+  initialDeposit = new Deposit(1234567890, 5000.00);
+  }
 }
